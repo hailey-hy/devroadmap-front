761c6b0f58febdbc0dac76f272593f7b34c6a313
hailey-hy <lillyine.hy@gmail.com>
2022-06-02 17:31:51 +0900

    메인페이지-로그인-소셜로그인 URL 수정

--- src/components/login/OAuthKakao.jsx
+++ src/components/login/OAuthKakao.jsx
@@ -1,4 +1,4 @@
-const CLIENT_ID = "2f9d11af41675d0d61c1687366d1a46d";
-const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+const CLIENT_ID = "99753ded86a228fda53902f70c39dee4";
+const REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
--- src/components/main/Status.jsx
+++ src/components/main/Status.jsx
@@ -8,7 +8,7 @@ const Status = (prop) => {
     <div className='container-status'>
         <div className="container-status-main">
         <Badge pill  background-color={prop}>Front</Badge>
-            <h5 id="status-d-day">다음 진도까지 {}일</h5>
+            <h5 id="status-d-day">현재 진도를 시작한지 {}일</h5>
         </div>
         <h5 id="status-percent">진도율 상위 {}%</h5>
     </div>
