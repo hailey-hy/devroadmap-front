97f744283d02f2f6258b0ce7bb7d35669ccf4e14
hailey-hy <lillyine.hy@gmail.com>
2022-06-23 05:18:44 +0900

    정원 기록 페이지 - CSS 수정 완료

--- src/components/record/Record.jsx
+++ src/components/record/Record.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, {useEffect, useState} from 'react'
 import './record.css'
 import Bubbles from './Bubbles';
-import paginationBasic from './PaginationBasic';
 import { Pagination } from 'react-bootstrap';
 
 
@@ -36,11 +35,14 @@ const Record = () => {
     };
     const totalrecord = record.length;
 
-    let active = 1;
+    let [active, setActive] = useState(1);
     let items = [];
     for (let number = 1; number <= Math.ceil(record.length / recordPerPage); number++) {
     items.push(
-        <Pagination.Item key={number} active={number === active} onClick={() => setCurrentPage(number)}>
+        <Pagination.Item key={number} active={number === active} onClick={() => {
+        setCurrentPage(number)
+        setActive(number)
+        }}>
         {number}
         </Pagination.Item>,
     );
@@ -49,7 +51,10 @@ const Record = () => {
   return (
     <div className='container-white container'>
         <h3 id="white-title">정원 기록</h3>
+        <div id="container-records">
         <Bubbles record={currentRecord(record)} loading={loading}></Bubbles>
+        </div>
+        
         <Pagination>{items}</Pagination>
     </div>
   )
