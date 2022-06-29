7df26c6608e441513f4052f5b673241c82e6175e
hailey-hy <lillyine.hy@gmail.com>
2022-06-29 23:36:55 +0900

    친구목록 페이지 - 1차 퍼블리싱 진행 중

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import './friendList.css'
+import { OverlayTrigger, Tooltip, Badge } from 'react-bootstrap'
+import {MdCancel} from 'react-icons/md'
 
 const FriendList = () => {
   return (
-    <div>FriendList</div>
+    <div id='friend'>
+        <div className='container-white container'>
+            <div id="friend-title-divider">
+                <h3 id="white-title">친구 목록</h3>
+                <OverlayTrigger
+                    overlay={
+                        <Tooltip id={`tooltip-top`}>
+                        <strong>친구 신청을 확인해 보세요!</strong>
+                        </Tooltip>
+                    }
+                >
+                    <div id="add-btn">+</div>
+                </OverlayTrigger>
+            </div>
+            <div id="container-friend-divider">
+                <div id="friend-list">
+                    {/* 추후 컴포넌트 분리 필요 */}
+                    <div className="friend">
+                        <Badge pill className="friend-field">front</Badge>
+                        <div className="friend-img">사진</div>
+                        <h5 className="friend-name">이름</h5>
+                        <h5 className='friend-progress'>진도율</h5>
+                        <MdCancel/>
+                    </div>
+                </div>
+                <div id="friend-util">
+                    <div className='util' id="util-search">
+                        테스트중
+                    </div>
+                    <div className='util' id="util-random">
+                        테스트중
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
