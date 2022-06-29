175d47e06f540b56369d80b3a7cefa2f188e53c2
hailey-hy <lillyine.hy@gmail.com>
2022-06-29 23:37:12 +0900

    푸터 - 생성

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer id='footer'>
+        <h5 class='footer-content'>Footer</h5>
+    </footer>
+  )
+}
+
+export default Footer
