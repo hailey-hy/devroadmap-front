c78af08ca2ae1fdd8fce982ef287a2fc497ca5df
hailey-hy <lillyine.hy@gmail.com>
2022-07-28 03:21:43 +0900

    axios 연결 오류 수정

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ import axios from 'axios'
 ReactDOM.render(<App/>, document.querySelector('#root'))
 
 // axios.defaults.baseURL = "http://ec2-43-200-80-56.ap-northeast-2.compute.amazonaws.com:8080/";
-axios.defaults.baseURL = "https://localhost:8080/";
-axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = "https://localhost:8080/";
+// axios.defaults.withCredentials = true;
