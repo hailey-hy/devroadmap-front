fcb5d8e8eb7245c15857f73d10f82a0844e722d1
hailey-hy <lillyine.hy@gmail.com>
2022-10-09 11:53:50 +0900

    내브 바 - 스크롤 동작을 감지하여 내브바를 닫는 기능 추가

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -56,9 +56,30 @@ const Nav = (props) => {
 
   const toggleMenu = () => {
     setMenu(isOpen => !isOpen);
-    // props.navOpen(!isOpen);
   }
 
+  //내비게이션 바가 열려 있을 때 스크롤을 내리면 내비게이션 바를 닫음
+
+const [scroll, setScroll] = useState(false);
+
+useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll); //clean up
+    };
+  }, []);
+
+const handleScroll = () => {
+	// 스크롤이 Top에서 50px 이상 내려오면 true값을 useState에 넣어줌
+    if(window.scrollY >= 40){
+      setScroll(true);
+      setMenu(false);
+      console.log(scroll)
+    }else{
+    // 스크롤이 50px 미만일경우 false를 넣어줌
+      setScroll(false);
+    }
+  };
   
 
   //로그아웃 함수
