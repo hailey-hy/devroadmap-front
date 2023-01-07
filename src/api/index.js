b25524488199e5412a7c8fff5fab3d365f0d0a03
hailey-hy <lillyine.hy@gmail.com>
2023-01-08 01:00:41 +0900

    chore: 백엔드 주소 수정

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 if(localStorage.getItem("user")){
     var instance = axios.create({
-        baseURL: "https://localhost:8080",
+        baseURL: "http://ec2-43-200-80-56.ap-northeast-2.compute.amazonaws.com:8080/",
         params: {
             Authorization: "Bearer " + localStorage.getItem("user")
         },
@@ -16,7 +16,7 @@ if(localStorage.getItem("user")){
     //회원 가입, 로그인 api 요청시 JWT 토큰 미포함
 
     var instance = axios.create({
-        baseURL: "https://localhost:8080",
+        baseURL: "http://ec2-43-200-80-56.ap-northeast-2.compute.amazonaws.com:8080/",
         method: 'get'
     })
 }
