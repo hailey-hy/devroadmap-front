07da266d8e3f62c3eabbac07ec1653cb6e927216
hailey-hy <lillyine.hy@gmail.com>
2023-07-14 16:05:37 +0900

    feat: 로컬스토리지에 tester로 저장했을 경우 Status 컴포넌트 내용 변경
    - 테스트 환경일 경우 front로 고정
    - 공부한 일수, 진도율 표시하지 않음

--- src/components/main/Status.jsx
+++ src/components/main/Status.jsx
@@ -1,69 +1,101 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Badge from 'react-bootstrap/Badge'
-import Button from 'react-bootstrap/Button'
-import { useSelector } from 'react-redux';
-import instance from '../../api';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { STATUS } from '../UI/Constants'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import { useSelector } from "react-redux";
+import instance from "../../api";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { STATUS } from "../UI/Constants";
 
 const Status = (props) => {
+  const [userType, setUserType] = useState("user");
+  useEffect(() => {
+    if (localStorage.getItem("tester")) {
+      setUserType("tester");
+    }
+  }, []);
 
   //공부 분야 관련
-  let field = useSelector((state) => state.field);
-  const friendField = props.friendField
-  const rate = useSelector((state) => state.progress);
-  const date = useSelector((state) => state.date);
-  console.log(props.friendField)
-  console.log(props.friend)
 
+  let rate = useSelector((state) => state.progress);
+  let date = useSelector((state) => state.date);
+  let field = useSelector((state) => state.field);
+  const friendField = props.friendField;
+
+  console.log(props.friendField);
+  console.log(props.friend);
 
   const isFront = () => {
-    if((field == 'front')){
-      return true
-    } else{
-      return false
+    if (field == "front") {
+      return true;
+    } else {
+      return false;
     }
-  }
+  };
 
   //진도율 관련
   const dispatch = useDispatch();
 
   useEffect(() => {
     instance({
-      url: '/progressrate',
-    }).then(response => {
-      console.log(response.data)
-      dispatch({type: 'load-progress', progress: response.data.progressRate, date: response.data.startDays})
-    })
-  }, [rate])
+      url: "/progressrate",
+    }).then((response) => {
+      console.log(response.data);
+      dispatch({
+        type: "load-progress",
+        progress: response.data.progressRate,
+        date: response.data.startDays,
+      });
+    });
+  }, [rate]);
 
-  if(props.friend === true){
-    console.log(props)
-    return(
-      <div className='container-status'>
-          <div className="container-status-main">
-          <Badge pill bg={props.friendField==='front' ? 'primary' : 'success'}>{props.friendField}</Badge>
-              
-          </div>
-      </div>
-    )
-  }
-  else {
+  if (props.friend === true) {
+    console.log(props);
     return (
-      <div className='container-status'>
-          <div className="container-status-main">
-          <Badge pill bg={isFront() ? 'primary' : 'success'}>{field}</Badge>
-              <h5 id="status-d-day">{STATUS.STUDY_FRONT}{date}{STATUS.STUDY_BACK}</h5>
-          </div>
-          <h5 id="status-percent">{STATUS.RATE}{rate}{STATUS.PERCENT}</h5>
+      <div className="container-status">
+        <div className="container-status-main">
+          <Badge
+            pill
+            bg={props.friendField === "front" ? "primary" : "success"}
+          >
+            {props.friendField}
+          </Badge>
+        </div>
       </div>
-      
-    )
+    );
+  } else if (userType === "tester") {
+    return (
+      <div className="container-status">
+        <div className="container-status-main">
+          <Badge pill bg="primary">
+            {field}
+          </Badge>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container-status">
+        <div className="container-status-main">
+          <Badge pill bg={isFront() ? "primary" : "success"}>
+            {field}
+          </Badge>
+          <h5 id="status-d-day">
+            {STATUS.STUDY_FRONT}
+            {date}
+            {STATUS.STUDY_BACK}
+          </h5>
+        </div>
+        <h5 id="status-percent">
+          {STATUS.RATE}
+          {rate}
+          {STATUS.PERCENT}
+        </h5>
+      </div>
+    );
   }
-  
-}
+};
 
-export default Status
+export default Status;
