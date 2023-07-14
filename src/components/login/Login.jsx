c088857d979c4d3f40d7eec06deff52af977e6b8
hailey-hy <lillyine.hy@gmail.com>
2023-07-14 10:36:06 +0900

    feat: 로그인 우회 해제

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,130 +1,128 @@
-import React, {useState, useEffect} from 'react'
-import Button from 'react-bootstrap/Button'
-import { useModal } from '../../hooks/useModal';
-import Form from 'react-bootstrap/Form';
-import './login.css'
-import instance from '../../api'
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import { useModal } from "../../hooks/useModal";
+import Form from "react-bootstrap/Form";
+import "./login.css";
+import instance from "../../api";
 import { useNavigate } from "react-router-dom";
-import Garden from '../garden/Garden';
-import { LOGIN_MSSAGES, LOGIN_PLACE_HOLDERS, DOMAINS, LOGIN_ALERT } from '../UI/Constants';
-import '../garden/garden.css'
-import { modals } from '../UI/modals/Modals';
+import Garden from "../garden/Garden";
+import {
+  LOGIN_MSSAGES,
+  LOGIN_PLACE_HOLDERS,
+  DOMAINS,
+  LOGIN_ALERT,
+} from "../UI/Constants";
+import "../garden/garden.css";
+import { modals } from "../UI/modals/Modals";
 
 const Login = (props) => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailDefault, setEmailDefault] = useState('')
-  const [result, setResult] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailDefault, setEmailDefault] = useState("");
+  const [result, setResult] = useState("");
 
   const handleEmail = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const handlePassword = (e) => {
-      setPassword(e.target.value)
-  }
+    setPassword(e.target.value);
+  };
 
   //이메일 직접입력 선택시 작동 함수
 
-  const [emailInput, setEmailInput] = useState('');
+  const [emailInput, setEmailInput] = useState("");
 
   const handleEmailInput = (e) => {
-    setEmailInput(e.target.value)
-  }
+    setEmailInput(e.target.value);
+  };
 
   const handleEmailDefault = (e) => {
-    if(e.target.value == 'user-input'){
+    if (e.target.value == "user-input") {
+      var target = document.getElementById("email-input");
+      console.log(target);
+      target.style.display = "block";
+      target.style.pointerEvents = "all";
 
-      var target = document.getElementById('email-input');
-      console.log(target)
-      target.style.display = 'block';
-      target.style.pointerEvents = 'all'
-
-      var target2 = document.getElementById('login-id-email');
-      target2.style.display = 'none';
-    } else{
-      setEmailDefault(e.target.value)
+      var target2 = document.getElementById("login-id-email");
+      target2.style.display = "none";
+    } else {
+      setEmailDefault(e.target.value);
     }
-    
-  }
-
-  
+  };
 
   // login 버튼 클릭 이벤트
   const onClickLogin = () => {
     //직접 입력된 이메일이 있을 경우 해당 입력값을 도메인으로 사용
-    if (emailInput.length > 0){
-      var domain = emailInput
+    if (emailInput.length > 0) {
+      var domain = emailInput;
+    } else {
+      var domain = emailDefault;
     }
-    else {
-      var domain = emailDefault
-    }
-
     instance({
-      method: 'post',
-      url: '/signin',
+      method: "post",
+      url: "/signin",
       params: {
-        "email" : email + '@' + domain,
-        "password" : password
-      }
-    }).then(response => {
-      console.log(response.headers['jwtToken'])
-  
-      window.localStorage.setItem("user", response.data);
-      console.log(window.localStorage.getItem("user"));
-      window.location.replace('/'); //강제 새로고침
-      
-    }).catch(error => {
-      console.error(error);
-      handleOpen();
-    });
-  }
+        email: email + "@" + domain,
+        password: password,
+      },
+    })
+      .then((response) => {
+        console.log(response.headers["jwtToken"]);
+
+        window.localStorage.setItem("user", response.data);
+        console.log(window.localStorage.getItem("user"));
+        window.location.replace("/"); //강제 새로고침
+      })
+      .catch((error) => {
+        console.error(error);
+        handleOpen();
+      });
+  };
 
   //회원가입 이동
 
   const navigate = useNavigate();
 
   const goJoin = () => {
-    navigate('/signup/mail');
-  }
-  
+    navigate("/signup/mail");
+  };
 
   //로그인 실패 모달
   const { openModal, closeModal } = useModal();
   const handleOpen = () => {
     openModal(modals.simple, {
-      title : LOGIN_ALERT.TITLE,
-      body : LOGIN_ALERT.BODY,
-      onClose : () => closeModal(modals.simple)
+      title: LOGIN_ALERT.TITLE,
+      body: LOGIN_ALERT.BODY,
+      onClose: () => closeModal(modals.simple),
     });
-  }
-  
+  };
+
   return (
     <>
       <div id="login-garden">
         <Garden login={true}></Garden>
       </div>
       <div className="container-login">
-          <h3 id='login-title'>{LOGIN_MSSAGES.LOGIN}</h3>
-          <div id='login-input-grid'>
-            <div className='login-lable-container'>
+        <h3 id="login-title">{LOGIN_MSSAGES.LOGIN}</h3>
+        <div id="login-input-grid">
+          <div className="login-lable-container">
             <Form.Control
-              className='login-id'
+              className="login-id"
               type="text"
               aria-describedby="passwordHelpBlock"
-              placeholder='이메일'
+              placeholder="이메일"
               value={email}
               onChange={handleEmail}
             />
-            <label id='email-lable'>@</label>
-            </div>
-            <div className='login-lable-container'>
-            <Form.Select 
-            aria-label="Default select example"
-            id='login-id-email'
-            value={emailDefault}
-            onChange={handleEmailDefault}
+            <label id="email-lable">@</label>
+          </div>
+          <div className="login-lable-container">
+            <Form.Select
+              aria-label="Default select example"
+              id="login-id-email"
+              value={emailDefault}
+              onChange={handleEmailDefault}
             >
               <option>{DOMAINS.CHOOSE}</option>
               <option value="naver.com">{DOMAINS.NAVER}</option>
@@ -136,35 +134,36 @@ const Login = (props) => {
 
             <Form.Control
               // className='login-id-email'
-              id='email-input'
+              id="email-input"
               type="text"
               placeholder={DOMAINS.USER_INPUT}
               value={emailInput}
               onChange={handleEmailInput}
-              name='email-input'
+              name="email-input"
             />
-            </div>
           </div>
-          
+        </div>
 
-          <Form.Control
-            className='login-input'
-            id='login-pw'
-            type="password"
-            aria-describedby="passwordHelpBlock"
-            placeholder={LOGIN_PLACE_HOLDERS.PW}
-            value={password}
-            onChange={handlePassword}
-          />
-      
-          <div>
-          {/* 로그인 오류 시 오류 메시지 출력 창 */}
-          </div>
-          <Button className='btn-login' onClick={onClickLogin}>{LOGIN_MSSAGES.LOGIN}</Button>
-          <h5 id='login-back' onClick={goJoin}>{LOGIN_MSSAGES.JOIN}</h5>
+        <Form.Control
+          className="login-input"
+          id="login-pw"
+          type="password"
+          aria-describedby="passwordHelpBlock"
+          placeholder={LOGIN_PLACE_HOLDERS.PW}
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <div>{/* 로그인 오류 시 오류 메시지 출력 창 */}</div>
+        <Button className="btn-login" onClick={onClickLogin}>
+          {LOGIN_MSSAGES.LOGIN}
+        </Button>
+        <h5 id="login-back" onClick={goJoin}>
+          {LOGIN_MSSAGES.JOIN}
+        </h5>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Login;
